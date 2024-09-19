f9301801fa058381b2afffa534031cbc0553f795
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2024-09-18 22:20:21 -0300

    feat(deps): add firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp(environment.firebaseConfig)
+    ),
+    provideAuth(() => getAuth()),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyCsig3cyAcD1U3n1xWhmgMi9uwzSgti2O4",
+        authDomain: "forumegb-web.firebaseapp.com",
+        projectId: "forumegb-web",
+        storageBucket: "forumegb-web.appspot.com",
+        messagingSenderId: "14899623411",
+        appId: "1:14899623411:web:2b7b60861853a4bf808e86",
+        measurementId: "G-VXN0XNS0KB"
+    }
+};
