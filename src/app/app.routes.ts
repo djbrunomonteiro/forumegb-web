703712dae6cb4cbcf7e2c8c19ff79ff185e70fc2
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2024-09-19 00:25:47 -0300

    feat(style): configuração theme color

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
+import { CadastroComponent } from './components/pages/cadastro/cadastro.component';
+import { LoginComponent } from './components/pages/login/login.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent 
+    },
+    {
+        path: 'cadastro',
+        component: CadastroComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
