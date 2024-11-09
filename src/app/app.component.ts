d7bdf521fe556663a82b6197bbd845d809230970
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2024-11-09 01:56:19 -0300

    feat: adiciona listem de rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { UserStoreService } from './store/user-store.service';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { AuthService } from './services/auth.service';
+import { UtilService } from './services/util.service';
 
 @Component({
   selector: 'app-root',
@@ -18,14 +18,15 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent implements OnInit {
 
-  auth = inject(AuthService)
-
+  auth = inject(AuthService);
+  #utils = inject(UtilService);
   title = 'egbhub-web';
   
 
   ngOnInit(){
+    this.#utils.listenCurrentNavState();
     this.auth.checkAuth();
   }
 
-  
+
 }
