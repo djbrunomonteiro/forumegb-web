ce546e500035eaeb3049a71535887209f119293e
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2025-04-08 23:41:23 -0300

    feat: adiciona novo recurso de rotas

---
+++ src/app/app.routes.server.ts
@@ -0,0 +1,19 @@
+import { RenderMode, ServerRoute } from '@angular/ssr';
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'perfil', // This renders the "/" route on the client (CSR)
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'politica-de-privacidade-e-termos-de-uso', // This page is static, so we prerender it (SSG)
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'posts/publicar', // This page requires user-specific data, so we use SSR
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: '**', // All other routes will be rendered on the server (SSR)
+    renderMode: RenderMode.Server,
+  },
+];
